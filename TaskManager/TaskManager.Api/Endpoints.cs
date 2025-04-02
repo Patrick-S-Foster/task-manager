7deb97874e4c2f1d9cdb6fdb33845e56016dae22
Patrick Foster <foster.s.patrick@gmail.com>
2025-04-02 16:28:25 +0200

    Implementation of remaining endpoints.

--- TaskManager/TaskManager.Api/Endpoints.cs
+++ TaskManager/TaskManager.Api/Endpoints.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TaskManager.Api.Model;
 using TaskManager.Common;
+using Task = TaskManager.Common.Task;
 
 namespace TaskManager.Api;
 
@@ -62,6 +63,157 @@ internal static class Endpoints
                 var task = entry.Entity.WithoutIdentityUser();
 
                 return Results.Created($"{applicationUrl}/tasks/{task.Id}", task);
+            }).RequireAuthorization();
+
+        app.MapPut("/update/{id:int}",
+            async (TaskDbContext db,
+                UserManager<IdentityUser> userManager,
+                ClaimsPrincipal claimsPrincipal,
+                int id,
+                [FromBody] Task task) =>
+            {
+                if (await userManager.GetUserAsync(claimsPrincipal) is not { } user)
+                {
+                    return Results.Unauthorized();
+                }
+
+                if (await db.Tasks.FindAsync(id) is not { } existingTask || existingTask.User != user)
+                {
+                    return Results.NotFound();
+                }
+
+                if (existingTask.State is TaskState.Completed)
+                {
+                    return Results.UnprocessableEntity();
+                }
+
+                existingTask.Name = task.Name;
+                existingTask.Description = task.Description;
+                existingTask.Notes = task.Notes;
+                existingTask.Branches = task.Branches;
+                await db.SaveChangesAsync();
+
+                return Results.NoContent();
+            }).RequireAuthorization();
+
+        app.MapPut("/start/{id:int}",
+            async (TaskDbContext db,
+                UserManager<IdentityUser> userManager,
+                ClaimsPrincipal claimsPrincipal,
+                int id) =>
+            {
+                if (await userManager.GetUserAsync(claimsPrincipal) is not { } user)
+                {
+                    return Results.Unauthorized();
+                }
+
+                if (await db.Tasks.FindAsync(id) is not { } existingTask || existingTask.User != user)
+                {
+                    return Results.NotFound();
+                }
+
+                if (existingTask.State is TaskState.Completed)
+                {
+                    return Results.UnprocessableEntity();
+                }
+
+                if (existingTask.State is TaskState.Created or TaskState.Paused)
+                {
+                    existingTask.LastStart = DateTime.Now;
+                }
+
+                existingTask.State = TaskState.Running;
+                await db.SaveChangesAsync();
+
+                return Results.Ok(existingTask.WithoutIdentityUser());
+            }).RequireAuthorization();
+
+        app.MapPut("/pause/{id:int}",
+            async (TaskDbContext db,
+                UserManager<IdentityUser> userManager,
+                ClaimsPrincipal claimsPrincipal,
+                int id) =>
+            {
+                if (await userManager.GetUserAsync(claimsPrincipal) is not { } user)
+                {
+                    return Results.Unauthorized();
+                }
+
+                if (await db.Tasks.FindAsync(id) is not { } existingTask || existingTask.User != user)
+                {
+                    return Results.NotFound();
+                }
+
+                if (existingTask.State is TaskState.Completed)
+                {
+                    return Results.UnprocessableEntity();
+                }
+
+                if (existingTask.State is TaskState.Running && existingTask.LastStart is { } lastStart)
+                {
+                    existingTask.Duration += DateTime.Now - lastStart;
+                }
+
+                existingTask.State = TaskState.Paused;
+                existingTask.LastStart = null;
+                await db.SaveChangesAsync();
+
+                return Results.Ok(existingTask.WithoutIdentityUser());
+            }).RequireAuthorization();
+
+        app.MapPut("/complete/{id:int}",
+            async (TaskDbContext db,
+                UserManager<IdentityUser> userManager,
+                ClaimsPrincipal claimsPrincipal,
+                int id) =>
+            {
+                if (await userManager.GetUserAsync(claimsPrincipal) is not { } user)
+                {
+                    return Results.Unauthorized();
+                }
+
+                if (await db.Tasks.FindAsync(id) is not { } existingTask || existingTask.User != user)
+                {
+                    return Results.NotFound();
+                }
+
+                if (existingTask.State is TaskState.Completed)
+                {
+                    return Results.UnprocessableEntity();
+                }
+
+                if (existingTask.State is TaskState.Running && existingTask.LastStart is { } lastStart)
+                {
+                    existingTask.Duration += DateTime.Now - lastStart;
+                }
+
+                existingTask.State = TaskState.Completed;
+                existingTask.LastStart = null;
+                await db.SaveChangesAsync();
+
+                return Results.Ok(existingTask.WithoutIdentityUser());
+            }).RequireAuthorization();
+
+        app.MapDelete("/delete/{id:int}",
+            async (TaskDbContext db,
+                UserManager<IdentityUser> userManager,
+                ClaimsPrincipal claimsPrincipal,
+                int id) =>
+            {
+                if (await userManager.GetUserAsync(claimsPrincipal) is not { } user)
+                {
+                    return Results.Unauthorized();
+                }
+
+                if (await db.Tasks.FindAsync(id) is not { } existingTask || existingTask.User != user)
+                {
+                    return Results.NotFound();
+                }
+
+                db.Tasks.Remove(existingTask);
+                await db.SaveChangesAsync();
+
+                return Results.NoContent();
             }).RequireAuthorization();
     }
 }
